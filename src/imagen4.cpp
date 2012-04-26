4865a0afa17597b2103d658da00cf0ba246c5119
algui91 <algui91@gmail.com>
2012-04-26 10:45:11 +0200

    Terminado, solo falta corregir un bug en escribir imagen

--- src/imagen4.cpp
+++ src/imagen4.cpp
@@ -7,29 +7,29 @@ using namespace std;
 
 void Imagen::crear(int f, int c) {
 
-		this->columnas = c;
+	this->columnas = c;
 
-		Celdas primero;
+	Celdas primero;
 
-		primero.fila = new unsigned char[columnas];
-		pt = &primero;
+	primero.fila = new unsigned char[this->columnas];
+	this->pt = &primero;
 
-		Celdas* ptCelda = &primero;
+	Celdas* ptCelda = &primero;
 
-		for (int i = 1; i < f; i++) {
-			Celdas* aux = new Celdas;
-			aux->fila = new unsigned char[columnas];
-			ptCelda->nextRow = aux;
-			ptCelda = aux;
-		}
-		delete ptCelda; //Ultimo elemento de la estructura //TODO, borrarlo
+	for (int i = 1; i < f; i++) {
+		Celdas* aux = new Celdas;
+		aux->fila = new unsigned char[this->columnas];
+		ptCelda->nextRow = aux;
+		ptCelda = aux;
+	}
+	ptCelda = 0; //Ultimo elemento de la estructura //TODO, borrarlo
 }
 
 //-------------------------
 
 int Imagen::get_filas(){
 	int filas = 0;
-	Celdas* aux = pt;
+	Celdas* aux = this->pt;
 
 	while(aux){
 		aux = aux->nextRow;
@@ -56,10 +56,10 @@ bool Imagen::leer_imagen(const char file[]) {
 			destruir();
 			crear(f, c);
 			//Cambio de estructura
-			Celdas* aux = pt;
+			Celdas* aux = this->pt;
 			for (int i = 0; i < f; i++){
-				for (int j = 0; j < columnas; j++)
-					aux->fila[j] = auxBuffer[i * columnas + j];
+				for (int j = 0; j < this->columnas; j++)
+					aux->fila[j] = auxBuffer[i * this->columnas + j];
 				aux = aux->nextRow;
 			}
 			aux = 0;
@@ -72,7 +72,7 @@ bool Imagen::leer_imagen(const char file[]) {
 }
 
 void Imagen::set_buffer(int i, int j, unsigned char v) {
-	Celdas* aux = pt;
+	Celdas* aux = this->pt;
 
 	while (i--)
 		aux = aux->nextRow;
@@ -84,9 +84,9 @@ void Imagen::set_buffer(int i, int j, unsigned char v) {
 //-------------------------
 
 unsigned char Imagen::get_buffer(int i, int j) const {
-	Celdas* aux = pt;
+	Celdas* aux = this->pt;
 
-	while (i++)
+	while (i-- && aux)
 		aux = aux->nextRow;
 	return aux->fila[j];
 }
@@ -108,13 +108,13 @@ void Imagen::destruir() {
 bool Imagen::escribir_imagen(const char file[]){
 	//Pasamos de la estructura de buffer2 a la simple
 	int filas = get_filas();
-	unsigned char* auxBuffer = new unsigned char[filas * columnas];
+	unsigned char* auxBuffer = new unsigned char[filas * this->columnas];
 
 	for (int i = 0; i < filas; i++)
-		for (int j = 0; j < columnas; j++)
-			auxBuffer[i*columnas + j] = get_buffer(i,j); //2d Array to 1D
+		for (int j = 0; j < this->columnas; j++)
+			auxBuffer[i*this->columnas + j] = get_buffer(i,j); //2d Array to 1D
 
-	if (EscribirImagenPGM(file, auxBuffer, filas, columnas))
+	if (EscribirImagenPGM(file, auxBuffer, filas, this->columnas))
 		return true;
 	delete[] auxBuffer;
 
